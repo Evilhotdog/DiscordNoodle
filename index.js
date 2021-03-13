b8c4559df2c65acacacf028e67eb4a1f38f5a904
Ruby <rubenaharris05@gmail.com>
2021-03-13 16:40:50 +0000

    Oh god .env wasn't in gitignore before

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ const path = require("path")
 const Discord = require("discord.js")
 const config = require("dotenv").config()
 const http = require("http");
+const {MongoClient} = require('mongodb');
+
+
 const app = express()
 const client = new Discord.Client();
+const mongo = new MongoClient(process.env.URI)
+
+async function run() {
+try {
+    await mongo.connect();
+
+    await listDatabases(mongo);
+ 
+} catch (e) {
+    console.error(e);
+} finally {
+    await mongo.close();
+}
+}
+run().catch(console.dir)
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+ 
+
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
