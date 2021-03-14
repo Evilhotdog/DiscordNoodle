11010b1317233831916008a917b086c9154397a1
Ruby <rubenaharris05@gmail.com>
2021-03-14 17:35:07 +0000

    Created code to iterate through guilds

--- index.js
+++ index.js
@@ -4,40 +4,70 @@ const Discord = require("discord.js")
 const config = require("dotenv").config()
 const http = require("http");
 const {MongoClient} = require('mongodb');
-
+const mongoose = require('mongoose');
+const { Attachment, Message, Channel, Guild } = require("./server/schemas.js");
+const e = require("express");
 
 const app = express()
 const client = new Discord.Client();
-const mongo = new MongoClient(process.env.URI)
-
-async function run() {
-try {
-    await mongo.connect();
-
-    await listDatabases(mongo);
- 
-} catch (e) {
-    console.error(e);
-} finally {
-    await mongo.close();
-}
-}
-run().catch(console.dir)
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
- 
+mongoose.connect(process.env.URI, {useNewUrlParser: true})
+  
+function updateGuilds() {
+    
+    const guilds = client.guilds
+    let guildObjects = []
+    guilds.cache.forEach(guild => {
+        const categories = guild.channels.cache.filter(c => c.type=="category")
+        let categoryObjects = []
+        categories.forEach(category => {
+            channelObjects = []
+            category.children.forEach((channel) => {
+                channelMessages = []
+                Guild.exists({id: guild.id}).then(result => {
+                    if (result) {
+                    console.log("___" + category.name)
+                    Guild.findOne({id: guild.id}, (err, doc) => {
+                        console.log("+++"+ doc)
+                        channelMessages = doc.channels.cache.filter(c => c.id == channel.id).messages
+                    })}
+                }) 
+                channelObjects.push({
+                    id: channel.id,
+                    name: channel.name,
+                    messages: channelMessages
+                })
+                
+            })
+            categoryObjects.push({
+                id: category.id,
+                name: category.name,
+                channels: channelObjects
+            })
+            })
+            guildObjects.push({
+                name: guild.name,
+                id: guild.id,
+                categories: categoryObjects
+            })
+        })
+        console.log(guildObjects)
+        console.log(guildObjects[0].categories)
+    }
 
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
+    updateGuilds()
 });
+
+
 
 client.on("message", (message) => {
     console.log(message.content)
+    console.log(typeof(message.author))
+
+    let mssg = new Message({author: message.author, content: message.content})
+    console.log(mssg)
 })
 
 
---
+++ server/schemas.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+
+const attachmentSchema = new Schema({
+    name: String,
+    uri: String,
+    mssgType: String,
+  })
+const Attachment = mongoose.model("Attachment", attachmentSchema)
+const MessageSchema = new Schema({
+    author: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: false
+    },
+    attachments: [attachmentSchema],
+    time: Date,
+    id: String
+
+  });
+const Message = mongoose.model("Message", MessageSchema)
+const channelSchema = new Schema({
+    name: String,
+    messages: [MessageSchema],
+    id: String
+})
+const Channel = mongoose.model("Channel", channelSchema)
+const guildSchema = new Schema({
+    name: String,
+    categories: [{name: String, channels: [channelSchema]}],
+    channels: [channelSchema],
+    id: String
+  }
+);
+const Guild = mongoose.model("Guild", guildSchema)
+
+module.exports = {Attachment, Message, Channel, Guild}
