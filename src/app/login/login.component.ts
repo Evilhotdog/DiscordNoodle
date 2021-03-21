2fc2c3570d93087d81ca797bb8fc4f7dcc69b34d
Phoebe Harris <aliceharris@murena.io>
2021-03-21 18:46:30 +0000

    Can now update front-end array on discord message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageServiceService } from '../message-service.service'
 import { LoginUser } from '../login-user'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +10,12 @@ import { LoginUser } from '../login-user'
 export class LoginComponent implements OnInit {
   username: String
   password: String
-  constructor(private socketService: MessageServiceService) { }
+  constructor(private socketService: MessageServiceService, private router: Router) {
+    this.socketService.loginSucceeded()
+    .subscribe(data => {router.navigate(['/main'], {state: {
+      guilds: JSON.stringify(data)
+    }})})
+  }
   model = new LoginUser("", "")
   ngOnInit(): void {
     
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { MessageServiceService } from "../message-service.service"
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  data: any = {};
+  routeState: any
+  constructor(private socketService: MessageServiceService, private router: Router) {
+    if (this.router.getCurrentNavigation().extras.state) {
+      this.routeState = this.router.getCurrentNavigation().extras.state;
+      
+      if (this.routeState) {
+        console.log(this.routeState)
+        this.data.guilds = JSON.parse(this.routeState.guilds)
+        console.log(this.data.guilds)
+      }
+    }
+    this.socketService.message()
+    .subscribe((message: any)=>{
+      console.log(message)
+      console.log(2)
+      let guilds = this.data.guilds
+      let guildIndex = guilds.findIndex(guild => guild.guild_id == message.guild_id)
+      if (message.category_id){
+        let categoryIndex = guilds[guildIndex].categories.findIndex(category => category.category_id == message.category_id).find
+        let channelIndex = guilds[guildIndex].categories[categoryIndex].channels.findIndex(channel => channel.channel_id == message.channel_id)
+        this.data.guilds[guildIndex].categories[categoryIndex].channels[channelIndex].messages.push(message)
+      } else {
+        let channelIndex = guilds[guildIndex].freeChannels.findIndex(channel => channel.channel_id == message.channel_id)
+        this.data.guilds[guildIndex].freeChannels[channelIndex].messages.push(message)
+      }
+    })
+    console.log("Constructor finished")
+  }
 
   ngOnInit(): void {
   }
-
+  trackByGuildId(index: number, guild: any): string {
+    return guild._id
+  }
+ 
 }
